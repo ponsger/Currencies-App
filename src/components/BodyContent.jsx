cf660409a1a73ec5afd26ae10eb21b975aed14c0
German <ponsger@Germans-MacBook-Pro.local>
2023-08-24 09:44:53 -0600

    Set news about cryptos in this web page

--- src/components/BodyContent.jsx
+++ src/components/BodyContent.jsx
@@ -1,8 +1,9 @@
 import '../css/BodyContent/bodyContent.css'
 import '../css/BodyContent/content.css'
 
-import React ,{ Suspense } from 'react';
+import React, { Suspense } from 'react';
 const Exchange = React.lazy(() => import('./Exchange'));
+const News = React.lazy(() => import('./News'));
 
 
 
@@ -10,6 +11,11 @@ function BodyContent() {
     return (
         <section className="bodyContent">
             <div className="content">
+                <div>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <News />
+                    </Suspense>
+                </div>
                 <div className='exchange-content'>
                     <Suspense fallback={<div>Loading...</div>}>
                         <Exchange />
--- src/data/apiKey.js
+++ src/data/apiKey.js
@@ -1,1 +1,2 @@
 export const apiKey="2546c171fa3a652b2256d5e64df09d14";
+export const apiKeyNews="eb6c5dbe36fa406bc37099a75388f8cc";
