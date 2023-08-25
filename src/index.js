da993ca42752f3974dc5d0ba22aa258439e39938
German <ponsger@Germans-MacBook-Pro.local>
2023-08-24 19:15:37 -0600

    Repaired the initial styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import '../public/reset.css'
+import '../public/index.css'
 
 const root =ReactDOM.createRoot(document.getElementById('root'));
 
